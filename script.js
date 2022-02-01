2c032ac3f67aa40758c02bf691d53c138d3f4022
Serafim <7620226@gmail.com>
2022-02-02 00:41:29 +0300

    feat: add changing tracks and images

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const playButton = document.querySelector(".animation");
 const leftButton = document.querySelector(".left");
 const rightButton = document.querySelector(".right");
+const currentBackground = document.querySelector(".background");
+const currentTrackImage = document.querySelector("#track-image");
 
 //!---------------------- buttons effects
 
@@ -25,6 +27,24 @@ function buttonReleased() {
   event.target.classList.remove("clicked");
 }
 
+//!----------------------- timeline controller
+
+const timeline = document.querySelector(".timeline");
+const progress = document.querySelector(".progress");
+timeline.addEventListener("click", timelineHandler);
+
+function timelineHandler(event) {
+  const clickCoordinateX = event.clientX;
+  const timelineLeft = event.target.offsetLeft;
+  const timelineRight = event.target.getBoundingClientRect().right;
+  const timelineWidth = event.target.getBoundingClientRect().width;
+  const progressCoordinateInside = clickCoordinateX - timelineLeft;
+
+  let progressCoordinateX =
+    event.clientX - event.target.offsetLeft - progress.offsetWidth / 2 + "px";
+  progress.style.left = progressCoordinateX;
+}
+
 //!-------------------- pause checker
 
 function isPaused() {
@@ -35,17 +55,79 @@ function isPaused() {
 
 //!----------------------- audio function
 
+let audio = new Audio();
+audio.src = "assets/audio/0.mp3";
+audio.volume = 0.1;
+
+let currentTrack = 0;
+let playlist = {
+  0: "assets/audio/0.mp3",
+  1: "assets/audio/1.mp3",
+};
+
 function audioOperator(event) {
-  if (event.target.parentElement === playButton) {
-    console.log("targeted");
+  //! play/pause
+
+  if (
+    event.target.parentElement === playButton ||
+    event.target === playButton
+  ) {
+    switch (isPaused()) {
+      case true:
+        audio.play();
+        break;
+      case false:
+        audio.pause();
+        break;
+    }
   }
 
   switch (event.target.id) {
+    //! previous track
     case "prev":
-      console.log("prev");
+      currentTrack--;
+
+      if (playlist[currentTrack]) {
+        audio.src = playlist[currentTrack];
+        if (isPaused()) {
+          audio.play();
+        }
+      } else {
+        currentTrack = Object.keys(playlist).length - 1;
+        audio.src = playlist[currentTrack];
+
+        if (isPaused()) {
+          audio.play();
+        }
+      }
+
+      currentBackground.style.backgroundImage = `url("assets/img/${currentTrack}.jpg")`;
+      currentTrackImage.src = `assets/img/${currentTrack}.jpg`;
+
       break;
+
+    //! next track
     case "next":
-      console.log("next");
+      currentTrack++;
+
+      if (playlist[currentTrack]) {
+        audio.src = playlist[currentTrack];
+
+        if (isPaused()) {
+          audio.play();
+        }
+      } else {
+        currentTrack = 0;
+        audio.src = playlist[0];
+
+        if (isPaused()) {
+          audio.play();
+        }
+      }
+
+      currentBackground.style.backgroundImage = `url("assets/img/${currentTrack}.jpg")`;
+      currentTrackImage.src = `assets/img/${currentTrack}.jpg`;
+
       break;
   }
 }

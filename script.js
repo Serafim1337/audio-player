0e6ec00ed2dc0fc1d541e58c62d6e103492b67a1
Serafim <7620226@gmail.com>
2022-02-01 21:40:56 +0300

    feat: basic layout

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const playButton = document.querySelector(".animation");
 const leftButton = document.querySelector(".left");
 const rightButton = document.querySelector(".right");
 
+//!---------------------- buttons effects
+
 playButton.addEventListener("click", playHandler);
+playButton.addEventListener("click", audioOperator);
 function playHandler() {
   document.querySelector("#first").classList.toggle("pause");
   document.querySelector("#second").classList.toggle("pause");
@@ -10,12 +13,39 @@ function playHandler() {
 
 leftButton.addEventListener("mousedown", buttonPressed);
 leftButton.addEventListener("mouseup", buttonReleased);
+leftButton.addEventListener("click", audioOperator);
 rightButton.addEventListener("mousedown", buttonPressed);
 rightButton.addEventListener("mouseup", buttonReleased);
+rightButton.addEventListener("click", audioOperator);
 
 function buttonPressed(event) {
   event.target.classList.add("clicked");
 }
 function buttonReleased() {
   event.target.classList.remove("clicked");
+}
+
+//!-------------------- pause checker
+
+function isPaused() {
+  return playButton.firstElementChild.classList.contains("pause")
+    ? true
+    : false;
+}
+
+//!----------------------- audio function
+
+function audioOperator(event) {
+  if (event.target.parentElement === playButton) {
+    console.log("targeted");
+  }
+
+  switch (event.target.id) {
+    case "prev":
+      console.log("prev");
+      break;
+    case "next":
+      console.log("next");
+      break;
+  }
 }
